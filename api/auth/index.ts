61eb43e48c32897dc1fd0fb187fc6b2bd0e98896
Rzq12 <riezqi.dhermatria@gmail.com>
2026-03-15 20:38:38 +0700

    fix(auth): enhance CORS headers and add user authorization for stats endpoint

--- api/auth/index.ts
+++ api/auth/index.ts
@@ -16,7 +16,7 @@ import {
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
